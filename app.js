490c235fa6e0648c728e9eff4e4e99e1768e7bd1
ahmedSam22 <58396761+ahmedSam22@users.noreply.github.com>
2022-02-14 16:51:15 +0200

    a

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
 
-const path = require("path")
+const path = require("path");
+const { default: App } = require("./src/App");
+const port = process.env.port || 3000;
 
-app.listen(3000,()=>{
+if(process.env.MODE_ENV === "production"){
+    app.use(express.static())
+}
+
+app.listen(port,()=>{
     console.log("tamam");
     })
